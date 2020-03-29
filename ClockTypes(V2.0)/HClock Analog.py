66751d9756d9beb98d9d35b84cdb123e185af121
CoolCat467 <52022020+CoolCat467@users.noreply.github.com>
2020-03-29 13:46:32 -0500

    Take out unneeded code and make code much safer
    Take out the wn.mainloop(), as it's not doing anything, hiding under your main loop and never being executed till the program quits, and make the code safer with the " if __name__ == '__main__': " thing, which if it runs as a module (no idea why you would even do that anyways) the program might break stuff. Also loops totally can use "_" as a counter, but "_" can also cause special things in python that you may not want to happen. Also fixed a bit of spacing and made the window title a bit better. Finally, I also made it more efficient by making the rim lines and hands into functions, instead of taking up about 6 times the space with very similar code.

--- ClockTypes(V2.0)/HClock Analog.py
+++ ClockTypes(V2.0)/HClock Analog.py
@@ -1,13 +1,16 @@
 #!/usr/bin/env python3
-# Analog clock for Holoclock
+# Analog clock for my Holoclock project for the
+# Instructables 2020 Clock Contest at
+# https://www.instructables.com/contest/clocks2020/
 # Created by BitMan64
 
 import turtle
 import time
+
 wn = turtle.Screen()
 wn.bgcolor("black")
-wn.setup(width=800,height=480)
-wn.title("HClock Analog")
+wn.setup(width=800, height=480)
+wn.title("Analog Clock")
 wn.tracer(0)
 
 pen = turtle.Turtle()
@@ -15,82 +18,72 @@ pen.hideturtle()
 pen.speed(0)
 pen.pensize(4)
 
-def Clock_Face(h,m,s,pen):
+def outerRimLines(pen, rng, start, length):
+    # Draw markers at the rim of the clock
     pen.up()
-    pen.goto(0,200)
+    pen.goto(0, 0)
     pen.setheading(180)
-    pen.color("green")
-    pen.pendown()
-    ##pen.circle(200)
+    rotation = 360 / rng
+    for i in range(rng):
+        pen.penup()
+        pen.fd(start)
+        pen.pendown()
+        pen.fd(length)
+        pen.penup()
+        pen.goto(0, 0)
+        pen.rt(rotation)
+    pen.up()
 
-
+def clockHand(pen, size, angle, linelen):
+    # Draw a clock hand
     pen.penup()
-    pen.goto(0,0)
-    pen.setheading(180)
+    pen.goto(0, 0)
+    pen.setheading(90)
+    pen.pensize(size)
+    pen.pendown()
+    pen.rt(angle)
+    pen.fd(linelen)
+    pen.penup()
 
-
-    for _ in range(12):
-        pen.fd(190)
-        pen.pendown()
-        pen.fd(20)
-        pen.penup()
-        pen.goto(0,0)
-        pen.rt(30)
-
-    for _ in range(60):
-        pen.fd(205)
-        pen.pendown()
-        pen.fd(5)
-        pen.penup()
-        pen.goto(0,0)
-        pen.rt(6)
+def Clock_Face(h, m, s, pen):
+    pen.up()
+    pen.goto(0, 200)
+    pen.down()
+    
+    # Hour lines
+    pen.color("green")
+    outerRimLines(pen, 12, 190, 20)
+    
+    # Minute lines
+    outerRimLines(pen, 60, 205, 5)
 
     ##Hour Hand
-    pen.pensize(4)
-    pen.penup()
-    pen.goto(0,0)
     pen.color("green")
-    pen.setheading(90)
-    angle = (h / 12) * -360
-    pen.rt(angle)
-    pen.pendown()
-    pen.fd(90)
+    angle = (m / 60) * -360
+    clockHand(pen, 4, angle, 90)
 
     ##Minute Hand
-    pen.pensize(4)
-    pen.penup()
-    pen.goto(0,0)
     pen.color("green")
-    pen.setheading(90)
-    angle = (m / 60) * -360
-    pen.rt(angle)
-    pen.pendown()
-    pen.fd(130)
+    angle = (h / 12) * -360
+    clockHand(pen, 4, angle, 130)
 
     ##Second Hand
-    pen.pensize(3)
-    pen.penup()
-    pen.goto(0,0)
     pen.color("red")
-    pen.setheading(90)
     angle = (s / 60) * -360
-    pen.rt(angle)
-    pen.pendown()
-    pen.fd(180)
+    clockHand(pen, 3, angle, 180)
 
-while True:
-    ##Get the current system time
-    h = int(time.strftime("%I"))
-    m = int(time.strftime("%M"))
-    s = int(time.strftime("%s"))
-    Clock_Face(h,m,s,pen)
+def run():
+    while True:
+        # Get the current system time
+        h = int(time.strftime("%I"))
+        m = int(time.strftime("%M"))
+        s = int(time.strftime("%s"))
+        Clock_Face(h, m, s, pen)
+        
+        wn.update()
+        time.sleep(0.5)
+        
+        pen.clear()
 
-    wn.update()
-    time.sleep(0.5)
-    
-    pen.clear()
-
-
-
-
-wn.mainloop()
+if __name__ == '__main__':
+    run()
